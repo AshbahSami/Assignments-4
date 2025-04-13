27171be93f52ffb1b5773a0ee6abe76e50b6d066
AshbahSami <154905813+AshbahSami@users.noreply.github.com>
2025-04-13 06:56:56 +0500

    Add files via upload

---
+++ 02_agreementbot.py
@@ -0,0 +1,7 @@
+def main():
+	a=input('What your favourite animal is?')
+	print(f'My fav animal is also {a}')
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
